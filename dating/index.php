22ce183bfc7403a8353939b7ff8dc0c909554d86
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-01-28 12:51:31 -0800

    added the dating and food-two assignments

---
+++ dating/index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * @author: Ryan H.
+ * @version: https://github.com/rynhndrcksn/dating/blob/main/index.php
+ * index.php is the controller for our F3 MVC
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require autoload file
+require_once ('vendor/autoload.php');
+
+// create an instance of the base class (fat-free framework)
+$f3 = Base::instance();
+
+// define a default route (home page)
+$f3->route('GET /', function() {
+	// create a new view, then sends it to the client
+	$view = new Template();
+	echo $view->render('views/home.html');
+});
+
+// run fat free HAS TO BE THE LAST THING IN FILE
+$f3->run();
