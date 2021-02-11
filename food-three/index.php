3c27dea12c025b797a955c9b08f35556cae86322
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-02-11 12:54:53 -0800

    food-three is now validating and working properly

--- food-three/index.php
+++ food-three/index.php
@@ -35,23 +35,30 @@ $f3->route('GET|POST /order', function($f3) {
 
 	// if the form has been submitted
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$userFood = prep_input($_POST['food']);
+		$userMeal = prep_input($_POST['meal']);
 		// gather info from order and validate it
-		if (validFood($_POST['food'])) {
-			$_SESSION['food'] = $_POST['food'];
+		if (validFood($userFood)) {
+			$_SESSION['food'] = $userFood;
 		} else {
 			$f3->set('errors["food"]', 'Food cannot be blank');
 		}
-		if (validFood($_POST['meal'])) {
-			$_SESSION['meal'] = $_POST['meal'];
+
+		if (validMeal($userMeal)) {
+			$_SESSION['meal'] = $userMeal;
+		} else {
+			$f3->set('errors["meal"]', 'Not a valid meal!');
 		}
 
 		// if there are no errors, redirect to /order2
 		if (empty($f3->get('errors'))) {
-			$f3-reroute('/order2');
+			$f3->reroute('/order2');
 		}
 	}
 
 	$f3->set('meals', getMeals());
+	$f3->set('userFood', isset($userFood) ? $userFood : "");
+	$f3->set('userMeal', isset($userMeal) ? $userMeal : "");
 
 	$view = new Template();
 	echo $view->render('views/order.html');
@@ -62,21 +69,26 @@ $f3->route('GET|POST /order', function($f3) {
 // define an order2 route
 $f3->route('GET|POST /order2', function($f3) {
 
-	$f3->set('condiments', getCondiments());
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if (isset($_POST['condiments'])) {
+			$userConds = $_POST['condiments'];
+			if (validConds($userConds)) {
+				$_SESSION['conds'] = implode(', ', $userConds);
+			} else {
+				$f3->set('errors["conds"]', 'Not a valid condiment!');
+			}
+		}
+		$f3->reroute('summary');
+	}
 
+	$f3->set('condiments', getCondiments());
 
 	$view = new Template();
 	echo $view->render('views/order2.html');
 });
 
 // define an order route
-$f3->route('POST /summary', function() {
-	// gather info from order2
-	if (isset($_POST['condiments'])) {
-		$_SESSION['condiments'] = implode(', ', $_POST['condiments']);
-	} else {
-		$_SESSION['condiments'] = 'none';
-	}
+$f3->route('GET|POST /summary', function() {
 
 	echo '<pre>';
 	var_dump($_SESSION);
@@ -84,6 +96,9 @@ $f3->route('POST /summary', function() {
 
 	$view = new Template();
 	echo $view->render('views/summary.html');
+
+	// clear the SESSION array
+	session_destroy();
 });
 
 // run fat free HAS TO BE THE LAST THING IN FILE
--- food-three/model/validate.php
+++ food-three/model/validate.php
@@ -7,12 +7,35 @@
  */
 
 /**
- * returns true if not empty
+ * returns true if not empty and contains only letters
  * @param $food
  * @return bool
  */
 function validFood($food): bool {
-	return !empty(prep_input($food));
+	return !empty(prep_input($food)) && ctype_alpha(prep_input($food));
+}
+
+/**
+ * returns true if $meal is a valid meal
+ * @param $meal
+ * @return bool
+ */
+function validMeal($meal): bool {
+	return in_array($meal, getMeals());
+}
+
+/**
+ * returns true if $conds contains valid condiments
+ * @param $conds
+ * @return bool
+ */
+function validConds($conds): bool {
+	foreach ($conds as $cond) {
+		if (!in_array($cond, getCondiments())) {
+			return false;
+		}
+	}
+	return true;
 }
 
 /**
