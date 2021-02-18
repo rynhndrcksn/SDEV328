c13839b4f30f9e0c21b9a16ae5701cf36c9cf23a
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-02-18 12:55:12 -0800

    changed food-three to be OOP and yeah, neat stuff

--- food-three/index.php
+++ food-three/index.php
@@ -9,71 +9,45 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// require autoload file
+require_once ('vendor/autoload.php');
+
 // create a session
 session_start();
 
-// require autoload file
-require_once ('vendor/autoload.php');
-require_once ('model/data-layer.php');
-require_once ('model/validate.php');
-
-// create an instance of the base class (fat-free framework)
+// instantiate the classes
 $f3 = Base::instance();
+$validator = new Validate();
+$dataLayer = new DataLayer();
+$order = new Order();
+$controller = new Controller($f3);
 
 // turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
 // define a default route (home page)
-$f3->route('GET /', function() {
-	// create a new view, then sends it to the client
-	$view = new Template();
-	echo $view->render('views/home.html');
+$f3->route('GET /', function() use ($controller) {
+	$controller->home();
 });
 
 // define an order route
-$f3->route('GET|POST /order', function($f3) {
-
-	// if the form has been submitted
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$userFood = prep_input($_POST['food']);
-		$userMeal = prep_input($_POST['meal']);
-		// gather info from order and validate it
-		if (validFood($userFood)) {
-			$_SESSION['food'] = $userFood;
-		} else {
-			$f3->set('errors["food"]', 'Food cannot be blank');
-		}
-
-		if (validMeal($userMeal)) {
-			$_SESSION['meal'] = $userMeal;
-		} else {
-			$f3->set('errors["meal"]', 'Not a valid meal!');
-		}
-
-		// if there are no errors, redirect to /order2
-		if (empty($f3->get('errors'))) {
-			$f3->reroute('/order2');
-		}
-	}
-
-	$f3->set('meals', getMeals());
-	$f3->set('userFood', isset($userFood) ? $userFood : "");
-	$f3->set('userMeal', isset($userMeal) ? $userMeal : "");
-
-	$view = new Template();
-	echo $view->render('views/order.html');
+$f3->route('GET|POST /order', function() use ($controller, $order, $dataLayer, $validator) {
+	$controller->order();
 });
 
 // we can only use POST if the form method is POST, otherwise we need to use GET as GET is used for typing in the
 // URL, hyperlinks, and most other things
 // define an order2 route
-$f3->route('GET|POST /order2', function($f3) {
+$f3->route('GET|POST /order2', function($f3) use ($order, $validator, $dataLayer) {
+
+	// TODO: Move all this stuff to controller.php
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		if (isset($_POST['condiments'])) {
 			$userConds = $_POST['condiments'];
-			if (validConds($userConds)) {
-				$_SESSION['conds'] = implode(', ', $userConds);
+			if ($validator->validConds($userConds)) {
+				// since our object is stored in $_SESSION, we can just set the condiments with implode
+				$_SESSION['order']->setCondiments(implode(', ', $userConds));
 			} else {
 				$f3->set('errors["conds"]', 'Not a valid condiment!');
 			}
@@ -81,7 +55,7 @@ $f3->route('GET|POST /order2', function($f3) {
 		$f3->reroute('summary');
 	}
 
-	$f3->set('condiments', getCondiments());
+	$f3->set('condiments', $dataLayer->getCondiments());
 
 	$view = new Template();
 	echo $view->render('views/order2.html');
--- food-three/model/validate.php
+++ food-three/model/validate.php
@@ -6,48 +6,63 @@
  * handles all of our validation functions for food app
  */
 
-/**
- * returns true if not empty and contains only letters
- * @param $food
- * @return bool
- */
-function validFood($food): bool {
-	return !empty(prep_input($food)) && ctype_alpha(prep_input($food));
-}
+class Validate
+{
+	// fields
+	private $_dataLayer;
 
-/**
- * returns true if $meal is a valid meal
- * @param $meal
- * @return bool
- */
-function validMeal($meal): bool {
-	return in_array($meal, getMeals());
-}
-
-/**
- * returns true if $conds contains valid condiments
- * @param $conds
- * @return bool
- */
-function validConds($conds): bool {
-	foreach ($conds as $cond) {
-		if (!in_array($cond, getCondiments())) {
-			return false;
-		}
+	function __construct()
+	{
+		$this->_dataLayer = new DataLayer();
 	}
-	return true;
-}
 
-/**
- * takes a parameter, strips any white spaces, strips \\'s and //'s, and converts any HTML to it's ASCII code.
- * is used on its own, but also acts as a helper function
- * @param $data
- * @return string
- */
-function prep_input($data): string{
-	$data = strtolower($data);
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+	/**
+	 * returns true if not empty and contains only letters
+	 * @param $food
+	 * @return bool
+	 */
+	function validFood($food): bool
+	{
+		return !empty($this->prep_input($food)) && ctype_alpha($this->prep_input($food));
+	}
+
+	/**
+	 * returns true if $meal is a valid meal
+	 * @param $meal
+	 * @return bool
+	 */
+	function validMeal($meal): bool
+	{
+		return in_array($meal, $this->_dataLayer->getMeals());
+	}
+
+	/**
+	 * returns true if $conds contains valid condiments
+	 * @param $conds
+	 * @return bool
+	 */
+	function validConds($conds): bool
+	{
+		foreach ($conds as $cond) {
+			if (!in_array($cond, $this->_dataLayer->getCondiments())) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * takes a parameter, strips any white spaces, strips \\'s and //'s, and converts any HTML to it's ASCII code.
+	 * is used on its own, but also acts as a helper function
+	 * @param $data
+	 * @return string
+	 */
+	function prep_input($data): string
+	{
+		$data = strtolower($data);
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
 }
