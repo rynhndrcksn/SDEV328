72fb32321e1ce4321380806b5d7aa2b583c44f71
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2021-03-02 19:26:20 -0800

    finished dating 4. user bio isn't sanitizing properly, which is weird but okay

---
+++ dating/controllers/controller.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+ * controller is our controller for the dating web app. This file contains methods that are called in our index.php
+ * PHP version: 7.3
+ * @author Ryan Hendrickson
+ * @version https://github.com/rynhndrcksn/dating
+ */
+class Controller
+{
+	// fields
+	private $_f3;
+	private $_validator;
+	private $_dataLayer;
+
+	public function __construct($f3)
+	{
+		$this->_f3 = $f3;
+		$this->_validator = new Validation();
+		$this->_dataLayer = new DataLayer();
+	}
+
+	/**
+	 * displays the home.html page
+	 */
+	function home()
+	{
+		// create a new view and send the user to home.html
+		$view = new Template();
+		echo $view->render('views/home.html');
+	}
+
+	/**
+	 * displays the first sign up page, handles validating, then information is saved to Member object
+	 */
+	function signUp1()
+	{
+		// set the gender radio buttons
+		$this->_f3->set('gens', $this->_dataLayer->getGens());
+
+		// gather user supplied information
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$userFirst = $_POST['fname'];
+			$userLast = $_POST['lname'];
+			$userAge = $_POST['age'];
+			$userGen = $_POST['gender'];
+			$userPhone = $_POST['phone'];
+
+			// validate first name
+			if (!$this->_validator->validName($userFirst)) {
+				$this->_f3->set('errors["fname"]', 'Not a valid first name');
+			}
+
+			// validate last name
+			if (!$this->_validator->validName($userLast)) {
+				$this->_f3->set('errors["lname"]', 'Not a valid last name');
+			}
+
+			// validate age
+			if (!$this->_validator->validAge($userAge)) {
+				$this->_f3->set('errors["age"]', 'Not a valid age');
+			}
+
+			// validate gender
+			if (isset($userGen)) {
+				if (!$this->_validator->validGender($userGen)) {
+					$this->_f3->set('errors["gender"]', 'Not a valid gender');
+				}
+			}
+
+			// validate phone
+			if (!$this->_validator->validPhone($userPhone)) {
+				$this->_f3->set('errors["phone"]', 'Not a valid phone number');
+			}
+
+			// if there are no errors, create + store our member object then redirect to sign-up-2
+			if (empty($this->_f3->get('errors'))) {
+
+				// check whether or not we have a premium member
+				if(isset($_POST["premium"])) {
+					$_SESSION['member'] = new PremiumMember($userFirst, $userLast, $userAge, $userGen, $userPhone);
+				} else {
+					$_SESSION['member'] = new Member($userFirst, $userLast, $userAge, $userGen, $userPhone);
+				}
+
+				$this->_f3->reroute('/sign-up-2');
+			}
+		}
+
+		$this->_f3->set('userFirst', isset($userFirst) ? $userFirst : "");
+		$this->_f3->set('userLast', isset($userLast) ? $userLast : "");
+		$this->_f3->set('userAge', isset($userAge) ? $userAge : "");
+		$this->_f3->set('userGen', isset($userGen) ? $userGen : "");
+		$this->_f3->set('userPhone', isset($userPhone) ? $userPhone : "");
+
+		// create a new view, then sends it to the client
+		$view = new Template();
+		echo $view->render('views/sign-up-1.html');
+	}
+
+	/**
+	 * displays the 2nd sign up page, validates user inputs, and reroutes to either the interest (if premium member) or
+	 * summary if not
+	 */
+	function signUp2()
+	{
+		// set the gender radio buttons and states
+		$this->_f3->set('gens', $this->_dataLayer->getGens());
+		$this->_f3->set('states', $this->_dataLayer->getStates());
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$userEmail = $_POST['email'];
+			$userState = $_POST['state'];
+			$userSeeking = $_POST['seeking'];
+			// neither of these are working to filter the input? It's the most bizarre thing
+			//$userBio = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
+			$userBio = $this->_validator->prep_input($_POST['biography']);
+
+			// validate email
+			if (!$this->_validator->validEmail($userEmail)) {
+				$this->_f3->set('errors["email"]', 'Not a valid email');
+			}
+
+			// validate state
+			if (!$this->_validator->validState($userState)) {
+				$this->_f3->set('errors["state"]', 'Not a valid state...');
+			}
+
+			// validate seeking
+			if (isset($userSeeking)) {
+				if (!$this->_validator->validGender($userSeeking)) {
+					$this->_f3->set('errors["seeking"]', 'Not a valid gender');
+				}
+			}
+
+			// if there are no errors, assign the user's email, state, seeking, and bio then redirect to sign-up-3 OR summary
+			if (empty($this->_f3->get('errors'))) {
+				// use the setters on
+				$_SESSION['member']->setEmail($userEmail);
+				$_SESSION['member']->setState($userState);
+				$_SESSION['member']->setSeeking($userSeeking);
+				$_SESSION['member']->setBio($userBio);
+
+				if ($_SESSION['member'] instanceof PremiumMember) {
+					$this->_f3->reroute('/sign-up-3');
+				} else {
+					$this->_f3->reroute('/summary');
+				}
+			}
+		}
+
+		$this->_f3->set('userEmail', isset($userEmail) ? $userEmail : "");
+		$this->_f3->set('userState', isset($userState) ? $userState : "");
+		$this->_f3->set('userSeeking', isset($userSeeking) ? $userSeeking : "");
+		$this->_f3->set('userBio', isset($userBio) ? $userBio : "");
+
+		// create a new view, then sends it to the client
+		$view = new Template();
+		echo $view->render('views/sign-up-2.html');
+	}
+
+	/**
+	 * displays the 3rd sign up page, validates it, then user is rerouted to summary
+	 */
+	function signUp3()
+	{
+		// set indoor and outdoor interests
+		$this->_f3->set('indoors', $this->_dataLayer->getIndoor());
+		$this->_f3->set('outdoors', $this->_dataLayer->getOutdoor());
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$userIndoors = $_POST['indoorInterests'];
+			$userOutdoors = $_POST['outdoorInterests'];
+
+			// validate indoor activities
+			if (isset($userIndoors)) {
+				if (!$this->_validator->validIndoor($userIndoors)) {
+					$this->_f3->set('errors["indoor"]', 'Not a valid indoor activity...');
+				}
+			}
+
+			// validate outdoor activities
+			if (isset($userOutdoors)) {
+				if (!$this->_validator->validOutdoor($userOutdoors)) {
+					$this->_f3->set('errors["outdoor"]', 'Not a valid outdoor activity...');
+				}
+			}
+
+			// if there are no errors, assign the interests to our object then redirect to summary
+			if (empty($this->_f3->get('errors'))) {
+				if (isset($userIndoors)) {
+					$_SESSION['member']->setIndoorInterests($userIndoors);
+				} else {
+					$_SESSION['member']->setIndoorInterests(array('no indoor activities'));
+				}
+				if (isset($userOutdoors)) {
+					$_SESSION['member']->setOutdoorInterests($userOutdoors);
+				} else {
+					$_SESSION['member']->setOutdoorInterests(array('no outdoor activities'));
+				}
+
+				$this->_f3->reroute('/summary');
+			}
+		}
+
+		$this->_f3->set('userIndoors', isset($userIndoors) ? $userIndoors : []);
+		$this->_f3->set('userOutdoors', isset($userOutdoors) ? $userOutdoors : []);
+
+		// create a new view, then sends it to the client
+		$view = new Template();
+		echo $view->render('views/sign-up-3.html');
+	}
+
+	function summary()
+	{
+		$view = new Template();
+		echo $view->render('views/summary.html');
+
+		// clear our $_SESSION
+		session_destroy();
+	}
+}
--- dating/index.php
+++ dating/index.php
@@ -9,197 +9,41 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// require autoload file
+require_once ('vendor/autoload.php');
+
+// create an instance of the base class (fat-free framework) this needs to be below autoload but before $controller
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+$controller = new Controller($f3);
+
 // create a session
 session_start();
 
-// require autoload file
-require_once ('vendor/autoload.php');
-$validator = new Validation();
-$dataLayer = new DataLayer();
-
-// create an instance of the base class (fat-free framework)
-$f3 = Base::instance();
-
 // define a default route (home page)
-$f3->route('GET /', function() {
-	// create a new view, then sends it to the client
-	$view = new Template();
-	echo $view->render('views/home.html');
+$f3->route('GET /', function() use ($controller) {
+	$controller->home();
 });
 
 // start our signup routes (1/3)
-$f3->route('GET|POST /sign-up-1', function($f3) use ($validator, $dataLayer) {
-	// set the gender radio buttons
-	$f3->set('gens', $dataLayer->getGens());
-
-	// gather user supplied information
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$userFirst = $_POST['fname'];
-		$userLast = $_POST['lname'];
-		$userAge = $_POST['age'];
-		$userGen = $_POST['gender'];
-		$userPhone = $_POST['phone'];
-
-		// validate first name
-		if ($validator->validName($userFirst)) {
-			$_SESSION['fname'] = $userFirst;
-		} else {
-			$f3->set('errors["fname"]', 'Not a valid first name');
-		}
-
-		// validate last name
-		if ($validator->validName($userLast)) {
-			$_SESSION['lname'] = $userLast;
-		} else {
-			$f3->set('errors["lname"]', 'Not a valid last name');
-		}
-
-		// validate age
-		if ($validator->validAge($userAge)) {
-			$_SESSION['age'] = $userAge;
-		} else {
-			$f3->set('errors["age"]', 'Not a valid age');
-		}
-
-		// validate gender
-		if (isset($userGen)) {
-			if ($validator->validGender($userGen)) {
-				$_SESSION['gender'] = $userGen;
-			} else {
-				$f3->set('errors["gender"]', 'Not a valid gender');
-			}
-		}
-
-		// validate phone
-		if ($validator->validPhone($userPhone)) {
-			$_SESSION['phone'] = $userPhone;
-		} else {
-			$f3->set('errors["phone"]', 'Not a valid phone number');
-		}
-
-		// if there are no errors, redirect to sign-up-2
-		if (empty($f3->get('errors'))) {
-			$f3->reroute('/sign-up-2');
-		}
-	}
-
-	$f3->set('userFirst', isset($userFirst) ? $userFirst : "");
-	$f3->set('userLast', isset($userLast) ? $userLast : "");
-	$f3->set('userAge', isset($userAge) ? $userAge : "");
-	$f3->set('userGen', isset($userGen) ? $userGen : "");
-	$f3->set('userPhone', isset($userPhone) ? $userPhone : "");
-
-	// create a new view, then sends it to the client
-	$view = new Template();
-	echo $view->render('views/sign-up-1.html');
+$f3->route('GET|POST /sign-up-1', function() use ($controller) {
+	$controller->signUp1();
 });
 
 // continue signup routes (2/3)
-$f3->route('GET|POST /sign-up-2', function($f3) use ($validator, $dataLayer) {
-	// set the gender radio buttons and states
-	$f3->set('gens', $dataLayer->getGens());
-	$f3->set('states', $dataLayer->getStates());
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$userEmail = $_POST['email'];
-		$userState = $_POST['state'];
-		$userSeeking = $_POST['seeking'];
-		$userBio = $validator->prep_input($_POST['biography']);
-
-		// validate email
-		if ($validator->validEmail($userEmail)) {
-			$_SESSION['email'] = $userEmail;
-		} else {
-			$f3->set('errors["email"]', 'Not a valid email');
-		}
-
-		// validate state
-		if ($validator->validState($userState)) {
-			$_SESSION['state'] = $userState;
-		} else {
-			$f3->set('errors["state"]', 'Not a valid state...');
-		}
-
-		// validate seeking
-		if (isset($userSeeking)) {
-			if ($validator->validGender($userSeeking)) {
-				$_SESSION['seeking'] = $userSeeking;
-			} else {
-				$f3->set('errors["seeking"]', 'Not a valid gender');
-			}
-		}
-
-		// validate biography
-		if (isset($userBio)) {
-			// since we sanitized the input earlier with prep_input, we don't need to worry about it here
-			$_SESSION['biography'] = $userBio;
-		}
-
-		// if there are no errors, redirect to sign-up-3
-		if (empty($f3->get('errors'))) {
-			$f3->reroute('/sign-up-3');
-		}
-	}
-
-	$f3->set('userEmail', isset($userEmail) ? $userEmail : "");
-	$f3->set('userState', isset($userState) ? $userState : "");
-	$f3->set('userSeeking', isset($userSeeking) ? $userSeeking : "");
-	$f3->set('userBio', isset($userBio) ? $userBio : "");
-
-	// create a new view, then sends it to the client
-	$view = new Template();
-	echo $view->render('views/sign-up-2.html');
+$f3->route('GET|POST /sign-up-2', function() use ($controller) {
+	$controller->signUp2();
 });
 
 // end of our signup routes (3/3)
-$f3->route('GET|POST /sign-up-3', function($f3) use ($validator, $dataLayer) {
-	// set indoor and outdoor interests
-	$f3->set('indoors', $dataLayer->getIndoor());
-	$f3->set('outdoors', $dataLayer->getOutdoor());
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$userIndoors = $_POST['indoorInterests'];
-		$userOutdoors = $_POST['outdoorInterests'];
-
-		// validate indoor activities
-		if (isset($userIndoors)) {
-			if ($validator->validIndoor($userIndoors)) {
-				$_SESSION['indoorInterests'] = $userIndoors;
-			} else {
-				$f3->set('errors["indoor"]', 'Not a valid indoor activity...');
-			}
-		}
-
-		// validate outdoor activities
-		if (isset($userOutdoors)) {
-			if ($validator->validOutdoor($userOutdoors)) {
-				$_SESSION['outdoorInterests'] = $userOutdoors;
-			} else {
-				$f3->set('errors["outdoor"]', 'Not a valid outdoor activity...');
-			}
-		}
-
-		// if there are no errors, redirect to sign-up-3
-		if (empty($f3->get('errors'))) {
-			$f3->reroute('/summary');
-		}
-	}
-
-	$f3->set('userIndoors', isset($userIndoors) ? $userIndoors : []);
-	$f3->set('userOutdoors', isset($userOutdoors) ? $userOutdoors : []);
-
-	// create a new view, then sends it to the client
-	$view = new Template();
-	echo $view->render('views/sign-up-3.html');
+$f3->route('GET|POST /sign-up-3', function() use ($controller) {
+	$controller->signUp3();
 });
 
 // sign up summary route
-$f3->route('GET|POST /summary', function() {
-	$view = new Template();
-	echo $view->render('views/summary.html');
-
-	// clear our $_SESSION
-	//session_destroy();
+$f3->route('GET|POST /summary', function() use ($controller) {
+	$controller->summary();
 });
 
 // run fat free HAS TO BE THE LAST THING IN FILE
